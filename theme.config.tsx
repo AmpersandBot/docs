532a6bfa25bd52e801680f4699bbec42a376a7dd
__Ciach0 <42491334+Ciach0@users.noreply.github.com>
2023-01-05 02:57:19 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>AmpersandBot</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://ampersandbot.xyz',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/ggvm5GKbev',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ampersandbot/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'AmpersandBot',
   },
 }
 
